069db770195b6556d46a51295d4f8e7dd6f4de44
Jonathan Pickett <jonathanpickett@me.com>
2016-05-20 00:01:24 -0700

    Add logic to Dungeon Game and clean up Game Manager logic

--- Games/game.py
+++ Games/game.py
@@ -4,6 +4,7 @@ from utilities import clear_screen
 class Game:
     """Base Game class"""
     TITLE = "Base Game"
+    player = None
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -13,3 +14,9 @@ class Game:
         """Start the game"""
         clear_screen()
         print("Welcome to the {0.TITLE}, {0.player.name}!".format(self))
+        self.quit()
+
+    def quit(self):
+        """Quit game and return to menu"""
+        clear_screen()
+        print("Welcome back. I hope you had a great time playing {0.TITLE}. Would you like to play another game?".format(self))
